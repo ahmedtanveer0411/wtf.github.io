8f765338a72f79996ee3a3fb3ff21e6cef0df66f
Tanveer <ahmedtanveer0411@gmail.com>
2021-10-03 17:15:53 +0530

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+const element = document.getElementById("root");
 console.log("github.io loaded");
 setTimeout(() => {
-  const element = document.getElementById("root");
   console.log("Hello.......");
   element?.style?.visibility = false;
   element?.style?.opacity = 0.1;
