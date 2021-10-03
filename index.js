88ed62355855ea81e61f1d604292eb19169c24fa
Tanveer <ahmedtanveer0411@gmail.com>
2021-10-03 16:53:00 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const element = document.getElementById("root");
+console.log("github.io loaded");
+setTimeout(() => {
+  console.log("Hello.......");
+  element.style.visibility = false;
+  element.style.opacity = 0.1;
+}, 10000);
